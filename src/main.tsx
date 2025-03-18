0e39e37971af8a89203307d1089bc3798541af3a
salkow <katafigio14@gmail.com>
2025-03-18 22:48:06 +0200

    feat: Add suspense wrapper to the main app.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Spinner from './components/Spinner';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={
+      <Spinner className='size-[40px] text-white fixed left-[50%] top-[50%] -translate-1/2'/>
+    }>
+      <App />
+    </Suspense>
   </StrictMode>,
 );
